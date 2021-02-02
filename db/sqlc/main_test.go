5f51ab97d8902e3bb4eca5b1ce551af6d0294dc8
zakiarsyad <zakiarsyad@gmail.com>
2021-02-02 07:00:32 +0700

    test: create TestMain db

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:postgres@localhost:5555/simple-bank?sslmode=disable"
+)
+
+var testQueris *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueris = New(conn)
+
+	os.Exit(m.Run())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zakiarsyad/simple-bank
 
 go 1.15
+
+require github.com/lib/pq v1.9.0
