770f0fdfa1e6da5d9b1e9ed36ff0df72960f4f94
zakiarsyad <zakiarsyad@gmail.com>
2021-02-05 07:16:03 +0700

    feat: add first api

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/zakiarsyad/simple-bank/db/sqlc"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server ans setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/account", server.createAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server pn a specific address
+func (server *Server) Start(addres string) error {
+	return server.router.Run(addres)
+}
+
+func errorResponse(e error) gin.H {
+	return gin.H{"error": e.Error()}
+}
