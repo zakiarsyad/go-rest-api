b3df028cce98279f7534225932cbd57ed8c8ad88
zakiarsyad <zakiarsyad@gmail.com>
2021-02-05 11:10:32 +0700

    feat: add get list account endpoint

--- api/account.go
+++ api/account.go
@@ -17,6 +17,11 @@ type getAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
@@ -54,6 +59,27 @@ func (server *Server) getAccount(ctx *gin.Context) {
 			return
 		}
 
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	account, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
--- api/server.go
+++ api/server.go
@@ -18,6 +18,7 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/account", server.createAccount)
 	router.GET("/account/:id", server.getAccount)
+	router.GET("/account/", server.listAccount)
 
 	server.router = router
 	return server
